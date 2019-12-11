051928e918e3bb83000cbc459d9ee946edc9fd63
wangchaogang <765483472@qq.com>
2019-12-11 14:09:02 +0800

    修改

--- js/main.js
+++ js/main.js
@@ -1,43 +1,59 @@
+(function($) {
 
+	"use strict";	
 
-   
-
-
-
-
-
-
-
-
-
-
-
-$('.project-list').mixItUp({
- 
-  effects: ['fade','rotateZ'],
-     easing: 'snap',
-}); 
-
-
-
-
-  
-	  	
-    // CounterUp
-	$(document).ready(function( $ ) {
-		if($("span.count").length > 0){	
-			$('span.count').counterUp({
-					delay: 10, // the delay time in ms
-			time: 1500 // the speed time in ms
-			});
-		}
+  	$(".main-menu a").click(function(){
+		var id =  $(this).attr('class');
+		id = id.split('-');
+		$('a.active').removeClass('active');
+    	$(this).addClass('active');
+		$("#menu-container .content").slideUp('slow');
+		$("#menu-container #menu-"+id[1]).slideDown('slow');		
+		$("#menu-container .homepage").slideUp('slow');
+		return false;
 	});
-	
-  	// Pretty Photo
-	$("a[rel^='prettyPhoto']").prettyPhoto({
-		social_tools: false
-	});	
 
 
+	$(".main-menu a.homebutton").click(function(){
+		$("#menu-container .content").slideUp('slow');
+		$("#menu-container .homepage").slideDown('slow');
+		$(".logo-top-margin").animate({marginLeft:'45%'}, "slow");
+		$(".logo-top-margin").animate({marginTop:'120px'}, "slow");
+		return false;
+	});
+
+	$(".main-menu a.aboutbutton").click(function(){
+		$("#menu-container .content").slideUp('slow');
+		$("#menu-container .about-section").slideDown('slow');
+		$(".logo-top-margin").animate({marginTop:'0'}, "slow");
+		$(".logo-top-margin").animate({marginLeft:'0'}, "slow");
+		return false;
+	});
+
+	$(".main-menu a.projectbutton").click(function(){
+		$("#menu-container .content").slideUp('slow');
+		$("#menu-container .gallery-section").slideDown('slow');
+		$(".logo-top-margin").animate({marginTop:'0'}, "slow");
+		$(".logo-top-margin").animate({marginLeft:'0'}, "slow");
+		return false;
+	});
+
+	$(".main-menu a.contactbutton").click(function(){
+		$("#menu-container .content").fadeOut();
+		$("#menu-container .contact-section").slideDown('slow');
+		$(".logo-top-margin").animate({marginTop:'0'}, "slow");
+		$(".logo-top-margin").animate({marginLeft:'0'}, "slow");
+		return false;
+	});
+
+	$('.toggle-menu').click(function(){
+        $('.show-menu').stop(true,true).slideToggle();
+        return false;
+    });
+
+    $('.show-menu a').click(function() {
+    	$('.show-menu').fadeOut('slow');
+    });
 
 
+})(jQuery);
